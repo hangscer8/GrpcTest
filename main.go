f4ac0abe1ee4b10fb1eb30bd2e0b4fd889fa7ddb
nathan <hangscer@hotmail.com>
2022-01-02 14:41:25 +0800

    run gateway

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	hello_grpc "GrpcTest/pb"
 	"context"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
+	"log"
 	"net"
+	"net/http"
 )
 
 type server struct {
@@ -17,8 +20,21 @@ func (receiver *server) SayHi(ctx context.Context, req *hello_grpc.Req) (*hello_
 }
 
 func main() {
+	go func() {
+		startHttpserver()
+	}()
 	lis, _ := net.Listen("tcp", ":15050")
 	s := grpc.NewServer()
 	hello_grpc.RegisterHelloGRPCServer(s, &server{}) //注册服务 如果有多个服务，则写多个
 	s.Serve(lis)                                     //八股文固定组合写法
+
+}
+
+func startHttpserver() {
+	//1. mux 三步走
+	//2. register
+	//3. http server
+	mux := runtime.NewServeMux()
+	hello_grpc.RegisterHelloGRPCHandlerServer(context.Background(), mux, &server{})
+	log.Fatalln(http.ListenAndServe("0.0.0.0:18088", mux))
 }
